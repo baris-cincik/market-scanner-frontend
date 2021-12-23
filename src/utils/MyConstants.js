92991161f435b49a829af8d5d14be234ceffea22
Baris Cincik <35776119+baris-cincik@users.noreply.github.com>
2021-12-23 12:28:38 +0300

    switched off dev mode

--- src/utils/MyConstants.js
+++ src/utils/MyConstants.js
@@ -1,6 +1,6 @@
-const IS_DEV_BACKEND = true;
+const IS_DEV_BACKEND = false;
 export const BACKEND_URL = IS_DEV_BACKEND
-  ? 'http://127.0.0.1:5000'
-  : 'https://<XXXXXXXX>.herokuapp.com';
+  ? 'http://localhost:5000/'
+  : 'https://market-scanner-backend.herokuapp.com/';
 export const EXCHANGES = ['binance', 'kucoin'];
 export const PAIRS = ['BTC-USDT', 'ETH-USDT'];
